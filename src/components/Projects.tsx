1d25d3100931c58a0b2b27f764136ac90f4b4f39
TharinDu DulanKa <tharindudulanka2020@gmail.com>
2025-01-12 21:06:28 +0530

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -25,7 +25,7 @@ const Projects = () => {
       image: 'https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
       tags: ['TypeScript', 'JavaScript', 'HTML', 'Prisma', 'TailwindCSS'],
       githubUrl: 'https://github.com/luci1927/multi-offset-printers',
-      liveUrl: '#',
+      liveUrl: '',
     },
     {
       title: 'Chat Application for Mobile',

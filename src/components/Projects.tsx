1f8bafca79e6e13b4ecabcb58d5886f9f66bc201
TharinDu DulanKa <tharindudulanka2020@gmail.com>
2025-07-09 13:44:16 +0530

    Final Commit

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -15,7 +15,7 @@ const Projects = () => {
         'A full-stack e-commerce platform with features like product management, shopping cart, secure payments, and real-time inventory tracking.',
       image: 'https://images.unsplash.com/photo-1557821552-17105176677c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2089&q=80',
       tags: ['PHP', 'JavaScript', 'HTML', 'MySQL', 'Other'],
-      githubUrl: 'https://github.com/luci1927/nizon-web',
+      githubUrl: '#b',
       liveUrl: '#',
     },
     {
@@ -24,7 +24,7 @@ const Projects = () => {
         'A portfolio website template with a modern design, smooth animations, and responsive layout built using React and Tailwind CSS.',
       image: 'https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
       tags: ['TypeScript', 'JavaScript', 'HTML', 'Prisma', 'TailwindCSS'],
-      githubUrl: 'https://github.com/luci1927/multi-offset-printers',
+      githubUrl: '#',
       liveUrl: '',
     },
     {
